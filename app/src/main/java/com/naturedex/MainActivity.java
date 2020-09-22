4cc4162619805075f303c353aa533151827bf5a1
Isaac <48636131+Moogamouth@users.noreply.github.com>
2020-09-21 17:32:13 -0700

    a

--- app/src/main/java/com/naturedex/MainActivity.java
+++ app/src/main/java/com/naturedex/MainActivity.java
@@ -3,12 +3,15 @@ package com.naturedex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
